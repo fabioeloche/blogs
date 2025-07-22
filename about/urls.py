4842725ed0abe71853ba929aebec4f8660f47f28
fabioeloche <hmll86f3@students.codeinstitute.net>
2025-07-22 23:09:41 +0100

    add about urls in about/urls.py

---
+++ about/urls.py
@@ -0,0 +1,11 @@
+"""
+URL configuration for the about application.
+"""
+from django.urls import path
+from . import views
+
+app_name = 'about'
+
+urlpatterns = [
+    path('', views.about_view, name='about'),
+]
